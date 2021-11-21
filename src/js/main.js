6690a7e57cf7797d0cb7be7c9603858b3902f8ed
EMILY SHIH <emilyyshih@gmail.com>
2021-11-21 14:06:55 -0500

    fonts

--- src/js/main.js
+++ src/js/main.js
@@ -112,3 +112,78 @@ triggers.forEach((trigger) => {
 modals.forEach((modal) => {
   modal.addEventListener("click", dismissModal);
 });
+
+// How long does it take to fall?
+var duration = 10000;
+
+// How often are particles are generated?
+var rate = 1000;
+
+// Get root document
+var root = document.documentElement;
+
+// Get the containing element
+var container = document.querySelector(".container");
+
+// Intialize variables but not giving a value yet
+var width;
+var height;
+
+// Get the dimensions and update CSS
+var dimensions = function() {
+  
+  // Get the width of the container
+  width = container.offsetWidth;
+
+  // Get the height of the container
+  height = container.offsetHeight;
+
+  // Set height in CSS
+  root.style.setProperty("--height", `${height}px`);
+}
+
+// Invoke dimensions
+dimensions();
+
+// Run dimensions on resize
+window.onresize = dimensions;
+
+// Function that emit partlce
+var emit = function() {
+  
+  // Create particle element
+  var particle = document.createElement("img");
+  
+  // TODO: Make source dynamic or psuedo random
+  // image_1, image_2, then just pick a random number between 1 and 5
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  
+  // Add image source to particle
+  particle.src = "src/img/coffee bean.png";
+  
+  // Add particle class and random rotation
+  particle.className = Math.random() >= 0.5 ? "particle left" : "particle right";
+  
+  // Add random left value so particle emit across the width of the container
+  var left = Math.floor(Math.random() * (width - 50));
+  
+  // Add the random left value to the particle
+  particle.style.left = `${left}px`;
+  
+  // Append the particle to the container
+  container.appendChild(particle);
+  
+  // Clean up particle
+  setTimeout(() => {
+    particle.remove();
+  }, duration);
+}
+
+// Emit particles with setInterval
+var interval = setInterval(emit, rate);
+
+// TODO: Stop animation when screen is not visible
+// https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
+// https://www.w3schools.com/jsref/met_win_clearinterval.asp
+
+// TODO: Increase emit rate and reset interval based on slide number
